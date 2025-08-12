bd28e2ac2650d9761a6e4358c094a7ddebebb9b9
RenzoQuispe <renzo123cd@gmail.com>
2025-08-12 17:01:28 -0500

    actualizar .env de ejemplo, configuracion docker y dependencias

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from routes.movies import movies_bp
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True , origins=["*"])  
 
@@ -12,4 +16,6 @@ def flask():
     return 'Server backend funcionando :D'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    port = int(os.getenv("SERVER_PORT", 5000))
+    print(f"Server ejecutándose en el puerto {port}")
+    app.run(host="0.0.0.0", port=port, debug=True)
