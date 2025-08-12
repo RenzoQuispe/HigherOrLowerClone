4af79afad73ceeff35accd8ecbd7046b4629428d
RenzoQuispe <renzo123cd@gmail.com>
2025-08-12 18:50:09 -0500

    actualizar routes/movies.py con las rutas /random_anime_movie y /random_horror_movie

--- backend/routes/movies.py
+++ backend/routes/movies.py
@@ -1,17 +1,28 @@
 from flask import Blueprint, jsonify
-from controllers.tmdb_service import fetch_random_movie
+from controllers.tmdb_service import get_random_movie, get_random_anime_movie, get_random_horror_movie
 
 movies_bp = Blueprint('movies', __name__)
-
+# Ratings: Movies
 @movies_bp.route('/random_movie', methods=['GET'])
 def random_movie():
     try:
-        movie = fetch_random_movie()  # Llamamos a la función que obtiene la película aleatoria
+        movie = get_random_movie()
         return jsonify(movie), 200
     except Exception as e:
         return jsonify({"error": "No se pudo obtener la película"}), 500
-
-@movies_bp.route('/ejemplo', methods=['GET'])
-def movieEjemplo():
-    return jsonify({'title': 'The Codes of War', 'rating': 6.102, 'poster': 'https://image.tmdb.org/t/p/w500/oXeiQAfRK90pxxhP5iKPXQqAIN1.jpg'})
-
+# Score: Anime Movies
+@movies_bp.route('/random_anime_movie', methods=['GET'])
+def random_anime_movie():
+    try:
+        movie = get_random_anime_movie()
+        return jsonify(movie), 200
+    except Exception as e:
+        return jsonify({"error": "No se pudo obtener la película anime"}), 500
+# Ratings: Horror Movies
+@movies_bp.route('/random_horror_movie', methods=['GET'])
+def random_horror_movie():
+    try:
+        movie = get_random_horror_movie()
+        return jsonify(movie), 200
+    except Exception as e:
+        return jsonify({"error": "No se pudo obtener la película de horror"}), 500
